edb2451f066921c0ebb9cd2f240111822c4c32f3
_frHaKtal_ <49241224+frHaKtal@users.noreply.github.com>
2023-02-11 16:27:22 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,2 @@
+apt-get install subfinder assetfinder amass lolcat gau uniq xargs massdns nuclei getjs urldedupe gf eyewitness apache2
+pip3 install JSFinder jsa jsscanner corsy takeover paramspider 
